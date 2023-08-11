0e66fb558c1aa7f3c2bb2b92f2f6a604cec5439e
y-watagashi <kinchan.yamamoto@gmail.com>
2023-08-11 21:38:26 +0900

    :fire: コードを整理
    - x, yのデータの配列の長さが一致するかのバリデーションを追加

--- detected_heatmap.py
+++ detected_heatmap.py
@@ -6,17 +6,40 @@ import matplotlib.cm as cm
 
 
 
-# 縦960, 横1280のうち，ランダムに1000点を選択し，(x, y)の形式で格納
-def generate_random_data():
-    x = np.random.randint(0, 1280, 1000)
-    y = np.random.randint(0, 960, 1000)
+def generate_random_data(data_length: int):
+    """縦960, 横1280のうちランダムに1000点を選択し(x, y)の形式で格納
+
+    Args:
+        data_length (int): データの個数
+    """
+    x = np.random.randint(0, 1280, data_length)
+    y = np.random.randint(0, 960, data_length)
     return x, y
 
 
-# ヒートマップ（グリッドタイプ）の作成，pngで保存
-def generate_heatmap(title, x_label, y_label, x, y):
-    # 返り値がタプルのため，figを明示的に指定している
-    fig, ax = plt.subplots()
+def validate_data_length(x: np.array, y: np.array):
+    """データの個数を確認（xとyの長さが一致していることを確かめる）
+
+    Args:
+        x (np.array): x座標の配列
+        y (np.array): y座標の配列
+    """
+    if len(x) != len(y):
+        raise ValueError('xとyのデータ数が異なります．')
+    
+
+def generate_heatmap(title: str, x_label: str, y_label: str, x: np.array, y: np.array):
+    """ヒートマップ（グリッドタイプ）作成し.pngで保存
+
+    Args:
+        title (str): グラフのタイトル（.pngに表示される）
+        x_label (str): 横軸のラベル
+        y_label (str): 縦軸のラベル
+        x (np.array): x座標の配列
+        y (np.array): y座標の配列
+    """
+    validate_data_length(x, y)
+    fig, ax = plt.subplots() # 返り値がタプルのため，figを明示的に指定している
     sns.heatmap(np.histogram2d(x, y, bins=[128, 96])[0], cmap='Reds')
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
@@ -26,8 +49,17 @@ def generate_heatmap(title, x_label, y_label, x, y):
     plt.show()
 
 
-# ヒートマップ（なめらか）の作成，pngで保存
-def generate_kde_heatmap(title, x_label, y_label, x, y):
+def generate_kde_heatmap(title: str, x_label: str, y_label: str, x: np.array, y: np.array):
+    """ヒートマップ（なめらか）を作成.pngで保存
+
+    Args:
+        title (str): グラフのタイトル（.pngに表示される）
+        x_label (str): x軸のラベル
+        y_label (str): y軸のラベル
+        x (np.array): x座標の配列
+        y (np.array): y座標の配列
+    """
+    validate_data_length(x, y)
     # ポイントをなめらかにするための設定
     xx,yy = np.mgrid[0:1280:1,0:960:1]
     positions = np.vstack([xx.ravel(),yy.ravel()])
