17b9029d11d7f28d14065b7cc96326f0f7838fcc
y-watagashi <kinchan.yamamoto@gmail.com>
2023-08-21 14:47:17 +0900

    🔥 ヒートマップと画像を重ねる機能を作成

--- detected_heatmap.py
+++ detected_heatmap.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import matplotlib.cm as cm
 
+from PIL import Image
+
 
 
 def generate_random_data(data_length: int):
@@ -49,7 +51,7 @@ def generate_heatmap(title: str, x_label: str, y_label: str, x: np.array, y: np.
     plt.show()
 
 
-def generate_kde_heatmap(title: str, x_label: str, y_label: str, x: np.array, y: np.array):
+def generate_kde_heatmap(title: str, x_label: str, y_label: str, x: np.array, y: np.array, image_path: str):
     """ヒートマップ（なめらか）を作成.pngで保存
 
     Args:
@@ -58,23 +60,26 @@ def generate_kde_heatmap(title: str, x_label: str, y_label: str, x: np.array, y:
         y_label (str): y軸のラベル
         x (np.array): x座標の配列
         y (np.array): y座標の配列
+        image_path (str): 画像のパス
     """
     validate_data_length(x, y)
     # ポイントをなめらかにするための設定
     xx,yy = np.mgrid[0:1280:1,0:960:1]
     positions = np.vstack([xx.ravel(),yy.ravel()])
     value = np.vstack([x,y])
-    # bw_methodはなめらかさの度合いを指定するパラメータ
-    kernel = gaussian_kde(value, bw_method=0.1)
+    kernel = gaussian_kde(value, bw_method=0.1) # bw_methodはなめらかさの度合いを指定するパラメータ
     f = np.reshape(kernel(positions).T, xx.shape)
     # ヒートマップを生成する
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.invert_yaxis()
-    ax.contourf(xx,yy,f, cmap=cm.jet)
+    ax.contourf(xx,yy,f, cmap=cm.jet, alpha=0.3) # alphaはグラフの透明度（0が透明，1が不透明）
     ax.set_title(title)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    im = Image.open(image_path)
+    im = np.array(im)
+    ax.imshow(im, alpha=1.0)
     plt.savefig(f'{title}.png')
     plt.show()
 
@@ -85,7 +90,7 @@ if __name__ == '__main__':
     x_label = 'X'
     y_label = 'Y'
     # x, y = generate_random_data()
-    x = np.random.randint(0, 100, 10)
-    y = np.random.randint(0, 100, 10)
+    x = np.random.randint(0, 1280, 1000)
+    y = np.random.randint(0, 960, 1000)
     # generate_heatmap(title, x_label, y_label, x, y)
-    generate_kde_heatmap(title, x_label, y_label, x, y)
+    generate_kde_heatmap(title, x_label, y_label, x, y, 'sample_image.png')
